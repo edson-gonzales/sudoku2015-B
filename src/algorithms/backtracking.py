779d4cd85ba42a65a99e981a962c669fce7460f9
ccatari <ekans.c.a@gmail.com>
2015-04-11 20:33:45 -0400

    Completed the backtracking implementation
    It was conflicts with the input of the solve function and added
    documentation on it.

--- src/algorithms/backtracking.py
+++ src/algorithms/backtracking.py
@@ -1,15 +1,19 @@
+from string import maketrans
 
 class Backtracking(object):
-	def __init__(self,grid):
-		self.grid = grid
+	def __init__(self):
 		self.UNASSIGNED = 0
+		self.DIMENSION = 9
 
 	def find_unassigned_location(self,grid,row,col):
-		""" Function that returns the possition of a cell with the value self.UNASSIGNED
-		@param grid[9][9]: the grid with the current sudoku values.
-		@param row: row number in the grid.
-		@param col: column number in the grid.
-		@return array[2]: position with the UNASSIGNED value or None in the case that the grid is already filled.
+		""" Function that returns the possition of a cell with the value 
+		self.UNASSIGNED
+
+		int[9][9] grid -- the grid with the current sudoku values.
+		int row -- row number in the grid.
+		int col -- column number in the grid.
+		return array[2]: position with the UNASSIGNED value or None in the case that
+			the grid is already filled.
 		"""
 	 	for row in range(len(grid)):
 	 		for col in range(len(grid[row])):
@@ -18,11 +22,14 @@ class Backtracking(object):
 	 	return None
 		
 	def used_in_row(self, grid, row, num):
-		"""Function that returns a boolean value when the *num* provided is repeated in the row.
-		@param grid[9][9]: the grid with the current sudoku values.
-		@param row: row number in the grid.
-		@param num: number to eval in the row.
-		@return boolean: return True when is already used in the row or False when is not used.	
+		"""Function that returns a boolean value when the *num* provided is repeated
+		in the row.
+
+		int[9][9] grid -- the grid with the current sudoku values.
+		int row -- row number in the grid.
+		int num -- number to eval in the row.
+		return boolean -- return True when is already used in the row or False when
+			is not used.	
 		"""
 	 	for col in range(len(grid[row])):
 	 		if grid[row][col] == num:
@@ -30,11 +37,14 @@ class Backtracking(object):
 	 	return False
 	
 	def used_in_col(self, grid, col, num):
-		""" Function that returns a boolean value when the *num* provided is repeated in the column.
-		@param grid[9][9]: the grid with the current sudoku values.
-		@param col: column number in the grid.
-		@param num: number to eval in the col.
-		@return boolean: return True when is already used in the column or False when is not used.
+		""" Function that returns a boolean value when the *num* provided is
+		repeated in the column.
+
+		int[9][9] grid -- the grid with the current sudoku values.
+		int col -- column number in the grid.
+		int num -- number to eval in the col.
+		return boolean -- return True when is already used in the column or False 
+			when is not used.
 		"""
 	 	for row in range(len(grid)):
 	 		if grid[row][col] == num:
@@ -42,12 +52,15 @@ class Backtracking(object):
 	 	return False
 	
 	def used_in_box(self, grid, box_start_row,box_start_col, num):
-		""" Function that returns a boolean value when the *num* provided is repeated in a box 3x3.
-		@param grid[9][9]: the grid with the current sudoku values.
-		@param box_start_row: row number in the grid where the box 3x3 starts.
-		@param box_start_col: column number in the grid where the box 3x3 starts
-		@param num: number to eval in the box.
-		@return boolean: return True when is already used in the box3x3 or False when is not used.
+		""" Function that returns a boolean value when the *num* provided is 
+		repeated in a box 3x3.
+
+		int[9][9] grid -- the grid with the current sudoku values.
+		int box_start_row -- row number in the grid where the box 3x3 starts.
+		int box_start_col -- column number in the grid where the box 3x3 starts
+		int num -- number to eval in the box.
+		return boolean -- return True when is already used in the box3x3 or False
+			when is not used.
 		"""
 	 	for row in range(3):
 	 		for col in range(3):
@@ -55,23 +68,26 @@ class Backtracking(object):
 	 				return True
 	 	return False 
 
-	##
-	# Function that eval a number *num* provided whether this is valid or not in the provided *position [row,col]*.
-	
 	def is_safe(self, grid, row, col, num):
-		""" Function that eval a number *num* provided whether this is valid or not in the provided *position [row,col]*.
-		@param grid[9][9]: the grid with the current sudoku values.
-		@param row: row number in the grid.
-		@param col: column number in the grid.
-		@param num: number to eval in the sudoku grid.
-		@return boolean: return True when the number is valid or False in other case
+		""" Function that eval a number *num* provided whether this is valid or not 
+		in the provided *position [row,col]*.
+
+		Keyword arguments
+		int[9][9] grid-- the grid with the current sudoku values.
+		int row -- row number in the grid.
+		int col -- column number in the grid.
+		int num -- number to eval in the sudoku grid.
+		return boolean -- return True when the number is valid or False in other case
 		"""
-	 	return not self.used_in_row(grid,row,num) and not self.used_in_col(grid,col,num) and not self.used_in_box(grid,row-row%3, col-col%3,num)
+	 	return  (not self.used_in_row(grid,row,num) 
+	 			and not self.used_in_col(grid,col,num) 
+	 			and not self.used_in_box(grid,row-row%3, col-col%3,num))
 
 	def solve_backtraking(self,grid):
 		""" Function that solve a sudoku game with the backtracking algorithm
-		@param grid[9][9]: the grid with the current sudoku values.
-		@return boolean: return True the Grid was solved and False when there is not solution.
+		int[9][9] grid -- the grid with the current sudoku values.
+		return boolean -- return True the Grid was solved and False when there is 
+			not solution.
 		"""
 		row = 0
 		col = 0
@@ -81,17 +97,37 @@ class Backtracking(object):
 		row = position[0]
 		col = position[1]
 		for number in range(1,10):
-			# print the numbers by empty cell print("("+str(row)+","+str(col)+") number"+str(number))
 			if (self.is_safe(grid,row,col,number)):
 				grid[row][col] = number
-				if(self.solve(grid)):
+				if(self.solve_backtraking(grid)):
 					return True
 				grid[row][col] = self.UNASSIGNED
 	 	return False
 
-	def solve(self,grid):
+	def convert_to_grid(self, grid_string):
+		""" Function that convert the provided string to a grid 9x9
+
+		string grid_string -- the string to convert
+		return int[9][9] -- a grid 9x9
+		"""
+		convert_dot_to_zero = maketrans('.','0')
+		grid_string = str(grid_string).translate(convert_dot_to_zero)
+		list_of_numbers = [int(n) for n in grid_string]
+		grid = [[0]*self.DIMENSION]*self.DIMENSION
+		for row in range(self.DIMENSION):
+			grid[row] = list_of_numbers[row*self.DIMENSION:(row+1)*self.DIMENSION]
+		return grid
+
+
+	def solve(self, grid_string):
+		""" Function that return a grid 9x9 with the sudoku game solved if this can
+		be solved or return none in other case.
+
+		string grid_string -- the string that contains the sudoku game in the format 
+		"x....xx............xx....x...xxx.......x....xxx..x.x..x.....xxx.......xx........."
+		"""
+		grid = self.convert_to_grid(grid_string)
 	 	if (self.solve_backtraking(grid)):
 	 		return grid
 	 	else:
 	 		return None
-	 		#raise Exception("Does not exist solution")
--- tests/backtracking_test.py
+++ tests/backtracking_test.py
@@ -11,82 +11,214 @@ class BacktrackingTest(unittest.TestCase):
 
 	def test_return_none_for_find_unassigned_location_of_a_3x3_grip_when_all_are_full(self):
 		grid = [[1,2,3],[2,3,1],[3,1,2]]
-		backtracking = Backtracking(grid)
+		backtracking = Backtracking()
 		self.assertIsNone(backtracking.find_unassigned_location(grid,0,0))	
 	def test_return_position_for_find_unassigned_location_of_a_3x3_grip_when_there_is_a_blank(self):
 		grid = [[1,2,3],[2,3,0],[3,1,2]]
-		backtracking = Backtracking(grid)
+		backtracking = Backtracking()
 		self.assertEqual([1,2],backtracking.find_unassigned_location(grid,0,0))	
 	def test_return_position_for_find_unassigned_location_of_a_3x3_grip_when_there_is_a_blank_at_the_end(self):
 		grid = [[1,2,3],[2,3,1],[3,1,0]]
-		backtracking = Backtracking(grid)
+		backtracking = Backtracking()
 		self.assertEqual([2,2],backtracking.find_unassigned_location(grid,0,0))
 	def test_return_position_for_find_unassigned_location_of_a_3x3_grip_when_there_is_more_than_one_blank(self):
 		grid = [[1,2,0],[2,0,1],[3,0,2]]
-		backtracking = Backtracking(grid)
+		backtracking = Backtracking()
 		self.assertEqual([0,2],backtracking.find_unassigned_location(grid,0,0))
 
 	def test_return_true_when_a_number_is_used_in_a_row(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertTrue(backtracking.used_in_row(grid,0,9))
 	def test_return_true_when_a_number_is_used_in_the_last_row(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertTrue(backtracking.used_in_row(grid,8,3))
 	def test_return_false_when_a_number_is_not_used_in_a_row(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,0,0],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,0,0],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertFalse(backtracking.used_in_row(grid,1,8))
 	def test_return_false_when_a_number_is_not_used_in_the_last_row(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,0,0,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,0,0,9]
+			]
+		backtracking = Backtracking()
 		self.assertFalse(backtracking.used_in_row(grid,8,3))
 
 	def test_return_true_when_a_number_is_used_in_a_col(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertTrue(backtracking.used_in_col(grid,0,9))
 	def test_return_true_when_a_number_is_used_in_the_last_col(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,0],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,0],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,0],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,0],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertTrue(backtracking.used_in_col(grid,8,3))
 	def test_return_false_when_a_number_is_not_used_in_a_col(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,0,0],[4,0,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,0,0],
+			[4,0,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertFalse(backtracking.used_in_col(grid,1,8))
 	def test_return_false_when_a_number_is_not_used_in_the_last_col(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,0],[1,3,8,9,4,7,2,5,0],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,0,0,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,0],
+			[1,3,8,9,4,7,2,5,0],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,0,0,9]
+			]
+		backtracking = Backtracking()
 		self.assertFalse(backtracking.used_in_col(grid,8,3))
 
 	def test_return_true_when_a_number_is_used_in_a_box(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertTrue(backtracking.used_in_box(grid,6,3,1))
 	def test_return_false_when_a_number_is_not_used_in_a_box(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,0,0],[4,0,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,0,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,0,0],
+			[4,0,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,0,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertFalse(backtracking.used_in_box(grid,6,3,8))
 	
 	def test_return_true_when_a_number_is_valid_in_a_sudoku_grid_position(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,0,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,0,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertTrue(backtracking.is_safe(grid,5,3,7))
 	def test_return_false_when_a_number_is_not_valid_in_a_sudoku_grid_position(self):
-		grid = [[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,0,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]]
-		backtracking = Backtracking(grid)
+		grid = [
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,0,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			]
+		backtracking = Backtracking()
 		self.assertFalse(backtracking.is_safe(grid,3,7,3))
 
 	def test_return_grid_solved_when_is_solved_a_valid_sudoku_grid(self):
-		grid = [[3,0,6,5,0,8,4,0,0],[5,2,0,0,0,0,0,0,0],[0,8,7,0,0,0,0,3,1],[0,0,3,0,1,0,0,8,0],[9,0,0,8,6,3,0,0,5],[0,5,0,0,9,0,6,0,0],[1,3,0,0,0,0,2,5,0],[0,0,0,0,0,0,0,7,4],[0,0,5,2,0,6,3,0,0]]
-		backtracking = Backtracking(grid)
-		self.assertEqual([[3,1,6,5,7,8,4,9,2],[5,2,9,1,3,4,7,6,8],[4,8,7,6,2,9,5,3,1],[2,6,3,4,1,5,9,8,7],[9,7,4,8,6,3,1,2,5],[8,5,1,7,9,2,6,4,3],[1,3,8,9,4,7,2,5,6],[6,9,2,3,5,1,8,7,4],[7,4,5,2,8,6,3,1,9]],backtracking.solve(grid))
-	def test_return_None_when_cannot_be_solved_a_sudoku_grid(self):
-		grid = [[3,0,6,5,0,8,4,0,0],[5,2,0,0,0,0,0,0,0],[0,8,7,0,0,0,0,3,1],[0,0,3,0,1,0,0,8,0],[9,0,0,8,6,3,0,0,5],[0,5,0,0,9,0,6,0,0],[1,3,0,0,0,0,2,5,0],[0,0,0,0,0,0,0,7,4],[0,0,5,2,0,6,3,9,2]]
-		backtracking = Backtracking(grid)
-		self.assertFalse(backtracking.solve(grid))
+		grid = "3.65.84..52........87....31..3.1..8.9..863..5.5..9.6..13....25......"\
+			"..74..52.63.."
 
+		backtracking = Backtracking()
+		self.assertEqual([
+			[3,1,6,5,7,8,4,9,2],
+			[5,2,9,1,3,4,7,6,8],
+			[4,8,7,6,2,9,5,3,1],
+			[2,6,3,4,1,5,9,8,7],
+			[9,7,4,8,6,3,1,2,5],
+			[8,5,1,7,9,2,6,4,3],
+			[1,3,8,9,4,7,2,5,6],
+			[6,9,2,3,5,1,8,7,4],
+			[7,4,5,2,8,6,3,1,9]
+			],backtracking.solve(grid))
+	def test_return_None_when_cannot_be_solved_a_sudoku_grid(self):
+		grid = "3.65.84..52........87....31..3.1..8.9..863..5.5..9.6..13....25......"\
+			"..74..52.6392"
+		backtracking = Backtracking()
+		self.assertIsNone(backtracking.solve(grid))
 
 if __name__ == '__main__':
     unittest.main()
